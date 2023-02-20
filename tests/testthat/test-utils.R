1dfda7830ca6dcceeccdab447795f40161bb9af8
dyfan.jones <23235152+DyfanJones@users.noreply.github.com>
2023-02-20 14:46:50 +0000

    extra checks for empty lists

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -10,6 +10,15 @@ test_that("check extra_docker_args", {
   )
 })
 
+test_that("check extra_docker_args no args", {
+  extra_args <- list()
+
+  expect_equal(
+    extra_docker_args(extra_args),
+    "."
+  )
+})
+
 test_that("check extra_docker_args single argument", {
   extra_args <- list(
     f = "hi",
@@ -31,5 +40,32 @@ test_that("check extra_docker_args multiple build-args", {
   expect_equal(
     extra_docker_args(extra_args),
     ". -f hi --build-args foo=bar --build-args baz=qux"
+  )
+})
+
+test_that("check regional_hostname", {
+  region <- c(
+    "cn-bar",
+    "us-gov-bar",
+    "us-iso-bar",
+    "us-isob-bar",
+    "bar"
+  )
+
+  actual <- sapply(
+    region,
+    smdocker:::regional_hostname,
+    service_name = "foo",
+    USE.NAMES = F
+  )
+  expect_equal(
+    actual,
+    c(
+      "foo.cn-bar.amazonaws.com.cn",
+      "foo.us-gov-bar.amazonaws.com",
+      "foo.us-iso-bar.c2s.ic.gov",
+      "foo.us-isob-bar.sc2s.sgov.gov",
+      "foo.bar.amazonaws.com"
+    )
   )
 })
